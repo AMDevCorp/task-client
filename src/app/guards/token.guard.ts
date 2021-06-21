5a869df13082801bf88dcbbb28bbe3f5bbf97259
Agustin Rodriguez <slay1991@hotmail.com>
2021-06-21 00:46:24 -0300

    fixed guard issue

--- src/app/guards/token.guard.ts
+++ src/app/guards/token.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {TokenService} from "../services/token/token.service";
 
@@ -8,13 +8,19 @@ import {TokenService} from "../services/token/token.service";
 })
 export class TokenGuard implements CanActivate {
 
-  constructor(private tokenService: TokenService) {
+  constructor(private tokenService: TokenService, private router: Router) {
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return !!this.tokenService.getTokenFromStorage();
+    if(this.tokenService.getTokenFromStorage()) {
+      return true;
+    } else {
+      localStorage.clear();
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
 
 }
